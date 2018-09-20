1ff687e6687bfbc1522488e66be0f88a163d56cf
Fulzele <Minal.Fulzele@nw.win.int.kn>
2018-09-20 14:43:38 +0100

    third passing  test

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -6,6 +6,8 @@ public class RomanNumerals {
     static String decimalToRoman(int decimal) {
         if(decimal==2) {
             return "II";
+        } else if (decimal == 3) {
+            return "III";
         }
         return "I";
     }
